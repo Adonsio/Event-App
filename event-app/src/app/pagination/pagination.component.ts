b1f9a598965626d4a5fd11d914af27ba86cf0350
Adonsio <dominik.reuting@gmail.com>
2021-04-09 15:30:52 +0200

    Updated Pagination to use proper type defs

--- event-app/src/app/pagination/pagination.component.ts
+++ event-app/src/app/pagination/pagination.component.ts
@@ -1,34 +1,15 @@
-import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import {Component, Input, Output, EventEmitter} from '@angular/core';
 import {PaginatedEvent} from '../paginated-event';
-import {EventService} from "../event-list/event.service";
 
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
   styleUrls: ['./pagination.component.scss']
 })
-export class PaginationComponent implements OnInit {
+export class PaginationComponent {
 
-  @Input() events: PaginatedEvent;
-  // tslint:disable-next-line:no-output-rename
-  @Output('prevPage') prevPage: EventEmitter<any> = new EventEmitter();
-  // tslint:disable-next-line:no-output-rename
-  @Output('nextPage') nextPage: EventEmitter<any> = new EventEmitter();
-  constructor(private eventService: EventService) {
-    // @ts-ignore
-    this.events = eventService.getEvents();
-
-
-  }
-  pPage(): any{
-    this.prevPage.emit();
-  }
-
-  nPage(): any{
-    this.nextPage.emit();
-  }
-
-  ngOnInit(): void {
-  }
+  @Input() events: PaginatedEvent | null = null;
+  @Output() prevPage: EventEmitter<void> = new EventEmitter();
+  @Output() nextPage: EventEmitter<void> = new EventEmitter();
 
 }
