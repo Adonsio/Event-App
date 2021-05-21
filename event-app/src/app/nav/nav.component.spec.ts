5975688c713e4dbffd3a2451eaf513f697d3fd7d
Adonsio <dominik.reuting@gmail.com>
2021-05-21 14:16:24 +0200

    default nav component test

--- event-app/src/app/nav/nav.component.spec.ts
+++ event-app/src/app/nav/nav.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavComponent } from './nav.component';
+import {HttpClientModule} from "@angular/common/http";
+import {RouterTestingModule} from "@angular/router/testing";
 
 describe('NavComponent', () => {
   let component: NavComponent;
@@ -8,6 +10,7 @@ describe('NavComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientModule, RouterTestingModule],
       declarations: [ NavComponent ]
     })
     .compileComponents();
